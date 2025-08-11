60eaf581bcb9a752a052714e55cee830351b4fe3
R Harish Kumar <rharish0909@gmail.com>
2025-08-11 14:48:22 +0530

    Added getters setters and then constructors to the student class

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,13 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("hello","world");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,LocalDate.of(2004, Month.JANUARY,9),
+						"rharish0909@gmail.com",
+						"Harish",
+				21));
 	}
 
 }
